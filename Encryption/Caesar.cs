29e73c491a3bfb3c01b01b249c19ecea55c320a1
Servan42 <charlot.servan@gmail.com>
2024-04-21 23:22:23 +0200

    feat: caesar

---
+++ Encryption/Caesar.cs
@@ -0,0 +1,31 @@
+﻿namespace Encryption
+{
+    internal class Caesar(string sourcePath, string encryptedPath, string decryptedPath)
+    {
+        public void Encrypt(int offset)
+        {
+            using var reader = new StreamReader(sourcePath);
+            using var writter = new StreamWriter(encryptedPath);
+
+            while (!reader.EndOfStream)
+            {
+                char c = (char)reader.Read();
+                c = (char)(c + offset);
+                writter.Write(c);
+            }
+        }
+
+        public void Decrypt(int offset)
+        {
+            using var reader = new StreamReader(encryptedPath);
+            using var writter = new StreamWriter(decryptedPath);
+
+            while (!reader.EndOfStream)
+            {
+                char c = (char)reader.Read();
+                c = (char)(c - offset);
+                writter.Write(c);
+            }
+        }
+    }
+}
---
+++ Encryption/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Encryption;
+using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // TODO https://code-maze.com/csharp-string-encryption-decryption/
+
+        var sw = Stopwatch.StartNew();
+
+        var caesar = new Caesar("input.txt", "cyph.txt", "decyph.txt");
+        caesar.Encrypt(-10);
+        caesar.Decrypt(-10);
+
+        sw.Stop();
+        Console.WriteLine(sw.Elapsed);
+        WriteSize("input.txt");
+        WriteSize("cyph.txt");
+        Console.WriteLine(EnsureIntegrity("input.txt", "decyph.txt"));
+    }
+
+    private static bool EnsureIntegrity(string inputFile, string outputFile)
+    {
+        var inputHash = SHA256.HashData(File.ReadAllBytes(inputFile));
+        var outputHash = SHA256.HashData(File.ReadAllBytes(outputFile));
+        if(inputHash.Length != outputHash.Length) return false;
+        for (int i = 0; i < inputHash.Length; i++)
+        {
+            if (inputHash[i] != outputHash[i])
+                return false;
+        }
+        return true;
+    }
+
+    private static void WriteSize(string path)
+    {
+        long byteCount = new FileInfo(path).Length;
+
+        if (byteCount >= 1024 * 1024)
+        {
+            double mbCount = (double)byteCount / (1024 * 1024);
+            Console.WriteLine($"Size: {mbCount:F2} MB ({path})");
+        }
+        else if (byteCount >= 1024)
+        {
+            double kbCount = (double)byteCount / 1024;
+            Console.WriteLine($"Size: {kbCount:F2} KB ({path})");
+        }
+        else
+        {
+            Console.WriteLine($"Size: {byteCount} bytes ({path})");
+        }
+    }
+}
